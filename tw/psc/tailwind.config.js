a0f43bc6435d4fee9deb80d59410e0ce5906cd54
Divya <divya@i47.in>
2023-07-27 13:26:23 +0530

    commit-page4changes

--- tw/psc/tailwind.config.js
+++ tw/psc/tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   content: ['*.html'],
 
+
+theme: {
+  extend: {
+    colors: {
+      custom8: 'D9D9D9', // Replace with your first gradient color
+      custom9: '#FFFFFF', // Replace with your second gradient color
+    },
+  },
+},
 }
